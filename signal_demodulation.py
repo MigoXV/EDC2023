c16c214c1c82b05c2c2c3406088999c5d9305f55
MigoXV <1695587905@qq.com>
2023-08-02 17:31:15 +0800

    测试模块构建且功能正常

--- signal_demodulation.py
+++ signal_demodulation.py
@@ -1,5 +1,24 @@
 # 导入必要的库，例如 numpy
 import numpy as np
+import matplotlib.pyplot as plt
+
+def am_demodulation():
+    print("am demodulation")
+
+def fm_demodulation():
+    pass
+
+def cw_demodulation():
+    pass
+
+def ask_demodulation():
+    pass
+
+def fsk_demodulation():
+    pass
+
+def psk_demodulation():
+    pass
 
 def demodulate_signal(signal_type, preprocessed_signal):
     """
@@ -22,14 +41,59 @@ def demodulate_signal(signal_type, preprocessed_signal):
     if signal_type == 'AM':
         # 示例: 对于 'AM' 信号，我们可能会进行解调
         # 这可以通过将信号转换为其绝对值来简单地实现
-        demodulated_signal = np.abs(preprocessed_signal)
+        demodulated_signal = am_demodulation()
 
     elif signal_type == 'FM':
         # 对于 'FM' 信号，我们可能需要进行更复杂的解调过程
         # 这可能需要通过傅里叶变换或其他频率分析技术来实现
         # 这里我们只是用一个占位符来代替真实的解调信号
-        demodulated_signal = np.array([])  # 占位符
+        demodulated_signal = fm_demodulation()
 
     # 以此类推，对于其他类型的信号，我们也可以添加相应的解调代码...
 
     return demodulated_signal
+
+if __name__=="__main__":
+
+    point=8192
+    n=np.array(range(point))
+    v0=0.1
+    v_omega=0.01
+    # ka=8
+    # ma=ka*v_omega/v0
+    ma=0.5
+    ka=ma*v0/v_omega
+    print(f'ka={ka:.2f}')
+    fs=10e6
+    fa=10e3
+    fcarrier=2e6
+    V_omega=0.01
+
+    T=fs/fa
+    print("T=",T)
+    omega0=2*np.pi/T
+    print("omega0=","{:.5f}".format(omega0))
+
+    modulating_wave=v_omega*np.sin(omega0*n)
+    plt.plot(modulating_wave)
+    plt.title('modulating_wave')
+    plt.show()
+
+    Tc=fs/fcarrier
+    print("Tc=",Tc)
+    omegac=2*np.pi/Tc
+    print("omegac=","{:.5f}".format(omegac))
+
+    carrier=v0*np.sin(omegac*n)
+    plt.plot(carrier)
+    plt.title('carrier')
+    plt.show()
+
+
+    modulated_wave=(v0+ka*modulating_wave)*carrier
+    plt.plot(modulated_wave)
+    plt.show()
+
+    demodulate_signal('AM',modulated_wave)
+
+
