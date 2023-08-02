0379654905aecf30aaf3148dd1d4e36a00116114
MigoXV <1695587905@qq.com>
2023-08-02 16:14:03 +0800

    GPT4生成函数

--- user_interface.py
+++ user_interface.py
@@ -1,59 +1,49 @@
-class UserInterface:
+# 导入必要的库
+import matplotlib.pyplot as plt
+
+def plot_demodulated_signal(demodulated_signal, signal_type):
     """
-    一个简单的命令行用户界面类，用于接收用户的输入并呈现结果。
+    将解调后的信号用matplotlib.pyplot画出。
+
+    参数:
+        demodulated_signal (np.array): 解调后的信号。
+        signal_type (str): 信号的类型。
+
+    返回:
+        None
     """
-    def __init__(self):
-        """
-        初始化 UserInterface 类的一个实例。
-        """
-        self.running = True
 
-    def prompt_for_signal(self):
-        """
-        提示用户输入信号类型和信号数据。
+    # 创建一个新的图形
+    plt.figure()
 
-        返回:
-            signal_type (str): 用户输入的信号类型。
-            signal_data (str): 用户输入的信号数据。
-        """
-        print("\n请输入信号类型:")
-        signal_type = input()
+    # 绘制解调后的信号
+    plt.plot(demodulated_signal)
 
-        print("\n请输入信号数据:")
-        signal_data = input()
+    # 设置图形的标题
+    plt.title(f'Demodulated signal of type {signal_type}')
 
-        return signal_type, signal_data
+    # 设置图形的x轴标签
+    plt.xlabel('Time')
 
-    def display_results(self, signal_type, parameters, demodulated_signal):
-        """
-        显示信号类型、参数估计和解调信号。
+    # 设置图形的y轴标签
+    plt.ylabel('Amplitude')
 
-        参数:
-            signal_type (str): 信号类型。
-            parameters (dict): 估计的信号参数。
-            demodulated_signal (str): 解调后的信号。
-        """
-        print("\n信号类型:", signal_type)
-        print("\n信号参数:")
-        for param, value in parameters.items():
-            print(f"{param}: {value}")
-        print("\n解调后的信号:", demodulated_signal)
+    # 显示图形
+    plt.show()
 
-    def run(self):
-        """
-        运行用户界面。
-        """
-        while self.running:
-            signal_type, signal_data = self.prompt_for_signal()
-            # 这里假设我们有一个名为 `process_signal` 的函数来处理信号
-            parameters, demodulated_signal = process_signal(signal_type, signal_data)
-            self.display_results(signal_type, parameters, demodulated_signal)
+def display_signal_info(signal_type, signal_params):
+    """
+    将信号的信息打印到控制台。
 
-            print("\n是否继续? (y/n)")
-            continue_answer = input()
-            if continue_answer.lower() != 'y':
-                self.running = False
+    参数:
+        signal_type (str): 信号的类型。
+        signal_params (dict): 信号的参数。
 
-if __name__ == "__main__":
-    ui = UserInterface()
-    ui.run()
+    返回:
+        None
+    """
+
+    print(f'Signal type: {signal_type}')
+
+    for param_name, param_value in signal_params.items():
+        print(f'{param_name}: {param_value}')
